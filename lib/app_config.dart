7401e183c513a0e95df3b582f15b6ddc0f4661e2
Deploy <deploy@cropdoctor.app>
2026-07-14 16:43:21 +0100

    Set OpenWeatherMap API key

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const String weatherApiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+  static const String weatherApiKey = '9f68998618a9662cb95ae4fd240c97a7';
   static const String backendUrl = 'https://crop-doctor-backend-production.up.railway.app';
 }
